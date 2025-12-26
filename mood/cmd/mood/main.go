4509377bbe3afcc19f1ca3a976ff7bfe336901a5
Maciej Kwapisz <ciamek.77@gmail.com>
2025-12-27 00:12:36 +0100

    Create mood main package

---
+++ mood/cmd/mood/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/ciameksw/mood-api/mood/internal/mood/config"
+	"github.com/ciameksw/mood-api/mood/internal/mood/logger"
+	"github.com/ciameksw/mood-api/mood/internal/mood/postgres"
+	"github.com/ciameksw/mood-api/mood/internal/mood/server"
+)
+
+func main() {
+	// Get logger
+	lgr := logger.GetLogger()
+
+	// Get config
+	cfg := config.GetConfig()
+
+	// Connect to Postgres
+	db, err := postgres.Connect(cfg.PostgresHost, cfg.PostgresPort, cfg.PostgresUser, cfg.PostgresPassword, cfg.PostgresDatabase, cfg.PostgresSSLMode)
+	if err != nil {
+		lgr.Error.Fatalf("Failed to connect to Postgres: %v", err)
+	}
+	defer db.Disconnect()
+
+	s := server.NewServer(lgr, cfg, db)
+	s.Start()
+}
