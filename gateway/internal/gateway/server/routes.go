861a4ebdc897881f885e3028178192286097a693
Maciej Kwapisz <ciamek.77@gmail.com>
2025-12-28 15:41:35 +0100

    Create routes.go

---
+++ gateway/internal/gateway/server/routes.go
@@ -0,0 +1,28 @@
+package server
+
+import "net/http"
+
+func (s *Server) setupAuthRouter(r *http.ServeMux) {
+	r.HandleFunc("POST /auth/register", s.handleRegister)                   // Register to the system
+	r.HandleFunc("POST /auth/login", s.handleLogin)                         // Login to get auth token
+	r.HandleFunc("GET /auth/user", s.authMiddleware(s.handleGetUser))       // Get logged user info
+	r.HandleFunc("PUT /auth/user", s.authMiddleware(s.handleUpdateUser))    // Update logged user info
+	r.HandleFunc("DELETE /auth/user", s.authMiddleware(s.handleDeleteUser)) // Delete logged user account
+}
+
+func (s *Server) setupMoodRouter(r *http.ServeMux) {
+	r.HandleFunc("POST /mood", s.authMiddleware(s.handleAddMood))               // Add new mood entry to the logged user
+	r.HandleFunc("GET /mood", s.authMiddleware(s.handleGetMoods))               // Get all mood entries of the logged user
+	r.HandleFunc("GET /mood/types", s.authMiddleware(s.handleGetMoodTypes))     // Get all available mood types
+	r.HandleFunc("GET /mood/summary", s.authMiddleware(s.handleGetMoodSummary)) // Get mood summary for the logged user
+	r.HandleFunc("PUT /mood", s.authMiddleware(s.handleUpdateMood))             // Update a mood entry of the logged user
+	r.HandleFunc("DELETE /mood/{id}", s.authMiddleware(s.handleDeleteMood))     // Delete a mood entry of the logged user
+}
+
+func (s *Server) setupAdviceRouter(r *http.ServeMux) {
+	r.HandleFunc("GET /advice", s.authMiddleware(s.handleGetAdvice)) // Get advice for the logged user
+}
+
+func (s *Server) setupQuoteRouter(r *http.ServeMux) {
+	// TODO: Quote service not implemented yet
+}
