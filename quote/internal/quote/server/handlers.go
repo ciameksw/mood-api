fbf6333b83b0791f3131640949cb96a13ce40055
Maciej Kwapisz <ciamek.77@gmail.com>
2025-12-28 21:52:30 +0100

    Use redis in handler

--- quote/internal/quote/server/handlers.go
+++ quote/internal/quote/server/handlers.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 )
@@ -8,10 +9,23 @@ import (
 func (s *Server) handleGetTodayQuote(w http.ResponseWriter, r *http.Request) {
 	s.Logger.Info.Println("Getting today's quote")
 
+	ctx := context.Background()
+
+	cachedQuote, err := s.RedisCache.GetTodayQuote(ctx)
+	if err == nil && cachedQuote != nil {
+		s.Logger.Info.Println("Quote found in cache")
+		s.writeJSON(w, cachedQuote, http.StatusOK)
+		return
+	}
+
 	resp, err := s.ExternalQuotesService.GetTodayQuote()
 	if err != nil {
 		s.handleError(w, "Failed to get today's quote", err, http.StatusInternalServerError)
 		return
+	}
+
+	if err := s.RedisCache.SetTodayQuote(ctx, resp); err != nil {
+		s.Logger.Error.Printf("Failed to cache quote: %v", err)
 	}
 
 	s.writeJSON(w, resp, http.StatusOK)
