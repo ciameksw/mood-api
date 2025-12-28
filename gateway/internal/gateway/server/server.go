0ed8be2598ea49b4b2117f24703c19c1e8e2e021
Maciej Kwapisz <ciamek.77@gmail.com>
2025-12-28 15:42:00 +0100

    Create server.go

---
+++ gateway/internal/gateway/server/server.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/ciameksw/mood-api/gateway/internal/gateway/config"
+	"github.com/ciameksw/mood-api/gateway/internal/gateway/services/auth"
+	"github.com/ciameksw/mood-api/pkg/logger"
+	"github.com/go-playground/validator/v10"
+)
+
+type Server struct {
+	Logger      *logger.Logger
+	Config      *config.Config
+	AuthService *auth.AuthService
+	Validator   *validator.Validate
+	httpServer  *http.Server
+}
+
+func NewServer(log *logger.Logger, cfg *config.Config) *Server {
+	return &Server{
+		Logger:      log,
+		Config:      cfg,
+		AuthService: auth.NewAuthService(cfg),
+		Validator:   validator.New(),
+	}
+}
+
+func (s *Server) Start() error {
+	r := http.NewServeMux()
+
+	s.setupAuthRouter(r)
+	s.setupMoodRouter(r)
+	s.setupAdviceRouter(r)
+	s.setupQuoteRouter(r)
+
+	r.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	addr := s.Config.ServerHost + ":" + s.Config.ServerPort
+	s.httpServer = &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+
+	s.Logger.Info.Printf("Starting server on %s", addr)
+	if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	s.Logger.Info.Println("Shutting down HTTP server...")
+	return s.httpServer.Shutdown(ctx)
+}
